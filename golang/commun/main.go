32827544c6a5d58d19c9b61911876dc0c4d71cc1
Ryan Liu <azhai@126.com>
2022-03-03 17:45:17 +0800

    测试不成功

--- golang/commun/main.go
+++ golang/commun/main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"context"
 	"io"
+	"net"
 	"sync"
 	"time"
 
 	"github.com/cloudwego/netpoll"
 )
 
-const RelayKey = "relay"
-var relayPool = sync.Pool {
-    New: func() any {
+const (
+	RelayKey   = "relay"
+	MaxPkgSize = 4096
+)
+
+var relayPool = sync.Pool{
+	New: func() any {
 		relay, _ := netpoll.DialConnection("tcp", "127.0.0.1:6379", time.Second)
 		// relay.SetOnRequest(handle)
 		return relay
-    },
+	},
 }
 
 func main() {
@@ -54,8 +59,44 @@ func finish(conn netpoll.Connection) (err error) {
 
 func handle(ctx context.Context, conn netpoll.Connection) (err error) {
 	relay := ctx.Value(RelayKey).(netpoll.Connection)
-	// go io.Copy(conn, relay) // 复制服务端回应
+
+	go pollCopy(conn.Writer(), relay.Reader()) // 复制服务端回应
 	// NOTICE: 与上面一行不能对调，否则无法知道客户端关闭了
-	// _, err = io.Copy(relay, conn) // 复制上报数据
+	_, err = pollCopy(relay.Writer(), conn.Reader()) // 复制上报数据
+
 	return nil
+}
+
+func ioCopy(writer, reader net.Conn) (int64, error) {
+	return io.Copy(writer, reader)
+}
+
+func pollCopy(writer netpoll.Writer, reader netpoll.Reader) (int64, error) {
+	pipe := make(chan *netpoll.LinkBuffer)
+
+	// writing
+	go func(pipe <-chan *netpoll.LinkBuffer) {
+		for {
+			select {
+			case pkg := <-pipe:
+				writer.Append(pkg)
+			default:
+				if writer.MallocLen() > 0 {
+					writer.Flush()
+				}
+			}
+		}
+	}(pipe)
+
+	// reading
+	for {
+		pkg, _ := reader.Slice(MaxPkgSize)
+		go func(pipe chan<- *netpoll.LinkBuffer, pkg netpoll.Reader) {
+			buf := netpoll.NewLinkBuffer()
+			data, _ := buf.ReadBinary(buf.Len())
+			buf.WriteBinary(data)
+			pipe <- buf
+			pkg.Release()
+		}(pipe, pkg)
+	}
 }
